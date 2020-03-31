079283adc496942c755ca9a7a32e2496a625b016
ANAMPEDIA <namdevel@users.noreply.github.com>
2020-03-31 21:38:23 +0700

    Create validate.nim

---
+++ validate.nim
@@ -0,0 +1,6 @@
+import src/spotify
+
+# can_accept_licenses_in_one_step = true (not registered)
+# "errors":{"email":" (email already registered in spotify)
+
+echo validateEmail("spotify@sbinary.pw")
